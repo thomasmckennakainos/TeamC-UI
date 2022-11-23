dcda96099f0ecb356633860a17d253625e0b8edf
Thomas McKenna <thomas.mckenna@kainos.com>
2022-11-23 11:35:57 +0000

    removed code from skeleton

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # TeamC-UI
+
+clone repo
+cd to root
+run 'npm install'
+run 'npm start'
+
+go to localhost:3000
--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const app = express()
 const nunjucks = require('nunjucks');
 const { v4: uuidv4 } = require('uuid');
 var cookieParser = require('cookie-parser')
-const isAuth = require('./MiddleWare/isAuth')
-const employee = require('./Database/Employee')
-const User = require('./Database/User');
 
 // app setup
 app.use(cookieParser());
@@ -27,11 +24,7 @@ app.get('/cookie', async (req, res) => {
     console.log(req.cookies)
 })
 
-//get method to render home page
-app.get('/', (req, res) => {
-    res.render('Home')
-})
-
+//method to redirect to error page
 app.get('*', function(req, res){
     res.status(404).render('ErrorPage');
   });
